4150b25979ef12437db1cc5b9a4f05f33802d1fe
Kengo Ozawa <kengorou@ozwspc.local>
2018-10-20 14:48:56 +0900

    wip

--- ResumeComp/app/controllers/resume_template_controller.rb
+++ ResumeComp/app/controllers/resume_template_controller.rb
@@ -3,4 +3,13 @@ class ResumeTemplateController < ApplicationController
   def index
   end
 
+  def download
+    PDFKit.configure do |config|
+      config.wkhtmltopdf = '/Users/kengorou/.rbenv/shims/wkhtmltopdf'
+    end
+    html = File.read("#{Rails.root}/app/views/resume_template/index.html.erb")
+    kit = PDFKit.new(html, :page_size => 'Letter')
+    send_data kit.to_pdf, filename: 'resume.pdf', type: 'application/pdf'
+  end
+
 end
--- ResumeComp/config/routes.rb
+++ ResumeComp/config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # resumeのtemplateを表示
   get "resume_template" => "resume_template#index"
+
+  # urlを叩けば上のresumeのtemplateをダウンロードしてくれる
+  get "download" => "resume_template#download"
 end
